d05101f088e2f8eeeeb34574cc1a8d959616a415
oleksa <shutt10@gmail.com>
2017-11-10 18:27:56 +0200

    + macoswindows installer

---
+++ BvcClient/installer_macx/build_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+QTDIR=~/Qt/5.8
+BUILD_DIR=../build-BvcClient-Desktop_Qt_5_8_0_clang_64bit_Release
+
+APPNAME=BvcClient
+
+$QTDIR/clang_64/bin/macdeployqt "$BUILD_DIR/$APPNAME.app" -dmg
+
+mv "$BUILD_DIR/$APPNAME.app" .
