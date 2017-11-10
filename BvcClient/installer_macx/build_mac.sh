ac01404066751074fc837096b7e15f8755f9fedf
oleksa <shutt10@gmail.com>
2017-11-10 19:29:19 +0200

    fix

--- BvcClient/installer_macx/build_mac.sh
+++ BvcClient/installer_macx/build_mac.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 QTDIR=~/Qt/5.8
-BUILD_DIR=../build-BvcClient-Desktop_Qt_5_8_0_clang_64bit_Release
+BUILD_DIR=../build-BvcClient-Desktop_Qt_5_8_0_clang_64bit-Release
 
 APPNAME=BvcClient
 
-$QTDIR/clang_64/bin/macdeployqt "$BUILD_DIR/$APPNAME.app" -dmg
+$QTDIR/clang_64/bin/macdeployqt $BUILD_DIR/$APPNAME.app -dmg
 
-mv "$BUILD_DIR/$APPNAME.app" .
+mv $BUILD_DIR/$APPNAME.dmg .
