1dc529ff32a4660085009903f0a47bd46c3c9fee
oleksa <shutt10@gmail.com>
2017-11-12 13:02:43 +0200

    fix

--- BvcClient/installer_macx/build_mac.sh
+++ BvcClient/installer_macx/build_mac.sh
@@ -13,10 +13,36 @@ BUNDLE_PATH=$BUILD_DIR/$APPNAME.app
 
 $QTDIR/macdeployqt $BUNDLE_PATH
 
-install_name_tool -change /usr/local/Cellar/ilmbase/2.2.0/lib/libIex-2_2.12.dylib @executable_path/../Frameworks/libIex-2_2.12.dylib "$BUNDLE_PATH/Contents/Frameworks/libImath-2_2.12.dylib"
-install_name_tool -change /usr/local/Cellar/ilmbase/2.2.0/lib/libIex-2_2.12.dylib @executable_path/../Frameworks/libIex-2_2.12.dylib "$BUNDLE_PATH/Contents/Frameworks/libIexMath-2_2.12.dylib"
-install_name_tool -change /usr/local/Cellar/ilmbase/2.2.0/lib/libIex-2_2.12.dylib @executable_path/../Frameworks/libIex-2_2.12.dylib "$BUNDLE_PATH/Contents/Frameworks/libIlmThread-2_2.12.dylib"
+echo ==== patch =================
+
+for f in $BUNDLE_PATH/Contents/Frameworks/lib*.dylib; do
+    F2L=`otool -L $f  | grep /usr/local/ | cut -f 2 | cut -f 1 -d ' '`
+    if [ ! -z "$F2L" ]; then
+        echo patch: $f
+        fb=`basename $f`
+        for f2 in $F2L; do
+            f2b=`basename $f2`
+            echo $fb $f2 $f2b
+            if [ "$fb" = "$f2b" ]; then
+                echo change id of $f
+                install_name_tool -id @executable_path/../Frameworks/$fb $f
+            else
+                install_name_tool -change $f2 @executable_path/../Frameworks/$f2b $f
+            fi
+        done
+    fi
+done
+
+echo ==== check begin =================
+
+for f in $BUNDLE_PATH/Contents/Frameworks/lib*.dylib; do
+    for f2 in `otool -L $f  | grep /usr/local/ | cut -f 2 | cut -f 1 -d ' '`; do
+        echo $f `basename $f2`
+    done
+done
+
+echo ==== check end =================
 
 $QTDIR/macdeployqt $BUNDLE_PATH -dmg
 
-mv $BUILD_DIR/BvcClient.dmg .
+mv $BUILD_DIR/$APPNAME.dmg .
