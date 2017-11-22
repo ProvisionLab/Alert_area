ae646f413e00de74ac2be2fbab58bd2359ebcf0c
oleksa <shutt10@gmail.com>
2017-11-22 13:56:48 +0200

    fix

--- backend_server/stop.sh
+++ backend_server/stop.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-if [ -f bvc_server.pid ]; then
+if [ ! -f bvc_server.pid ]; then
+    echo no runing server found
+    exit 1
+fi
 
 echo stoping...
 
 kill -HUP `cat bvc_server.pid`
 kill `cat bvc_server.pid`
 
-rm bvc_server.pid
+#rm bvc_server.pid
 
-fi
+while [ -f bvc_server.pid ]
+do
+  sleep 1
+done
 
+exit 0
