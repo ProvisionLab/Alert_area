03d35cc82be0283fee6e151bbba5e36d5e2586d1
oleksa <shutt10@gmail.com>
2017-11-22 15:40:48 +0200

    fix

--- reco_module/rog_client.py
+++ reco_module/rog_client.py
@@ -11,7 +11,7 @@ class RogClient(object):
         pass
 
     def post_alert(self, alert: AlertObject):
-        
+
         logging.debug(alert.as_dict())
 
         res = self.do_post(alert)
@@ -28,9 +28,10 @@ class RogClient(object):
 
             if not self.is_request_succeeded(res):
                 return False
-    
-        logging.info('rog post alert, type: %s, camera: [%d] \'%s\'', alert.alert_type, alert.camera_id, alert.camera_name)
-        
+
+        logging.info('rog post alert, type: %s, camera: [%d] \'%s\'',
+                     alert.alert_type, alert.camera_id, alert.camera_name)
+
         return True
 
     ###################################
@@ -40,7 +41,10 @@ class RogClient(object):
 
     def do_auth(self):
         r = requests.post('{0}/api/v2/sessions'.format(reco_config.rogapi_url),
-                            json={'session': {'email': reco_config.rogapi_username, 'password': reco_config.rogapi_password}})
+                          json={'session': {
+                              'email': reco_config.rogapi_username,
+                              'password': reco_config.rogapi_password
+                          }})
 
         if not self.is_request_succeeded(r.status_code):
             logging.error("rog auth failed, status: %d", r.status_code)
@@ -52,20 +56,20 @@ class RogClient(object):
         return True
 
     def do_post(self, alert: AlertObject):
-        
+
         if self.access_token is None:
             return 401  # auth required
 
         #print('do_post: ', payload)
         #return 200
-        
+
         r = requests.post('{0}/api/v2/bvc_alert'.format(reco_config.rogapi_url),
-                            headers={'Authorization': '{0}'.format(self.access_token)},
-                            json=alert.as_dict())
+                          headers={'Authorization': '{0}'.format(self.access_token)},
+                          json=alert.as_dict())
 
         if not self.is_request_succeeded(r.status_code):
-            logging.error("rog post alert failed, status: %d", r.status_code)
+            logging.error("rog post alert failed, camera: [%d], status: %d", alert.camera_id, r.status_code)
             logging.debug("rog post alert response data: %s", r.json())
-        
+
         return r.status_code
  
