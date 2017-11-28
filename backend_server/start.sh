f51e90840e0a86b904e1bcfc4845cd9c25893695
oleksa <shutt10@gmail.com>
2017-11-28 21:33:05 +0200

    fix

--- backend_server/start.sh
+++ backend_server/start.sh
@@ -8,6 +8,9 @@ fi
 
 echo starting
 
+# start MongoDB
+sudo service mongodb start
+
 # start as daemon
 gunicorn3 bvc_server:app -b 0.0.0.0:5000 -p bvc_server.pid -D \
     --access-logfile bvc_access.log \
